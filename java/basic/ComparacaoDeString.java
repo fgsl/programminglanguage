de44cd81d2f4eac198ddf161fedcccabb1a4a5c4
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2022-04-05 09:45:07 -0300

    String comparison samples #6

---
+++ java/basic/ComparacaoDeString.java
@@ -0,0 +1,10 @@
+class ComparacaoDeString {
+    public static void main(String[] args) {
+       String texto = "umafrase";
+       if (texto == "umafrase") {
+           System.out.println("O texto é uma frase");
+       } else {
+           System.out.println("O texto não é uma frase");     
+       }
+    }
+}
---
+++ java/basic/ComparacaoDeStringGarantida.java
@@ -0,0 +1,10 @@
+class ComparacaoDeStringGarantida {
+    public static void main(String[] args) {
+       String texto = "umafrase";
+       if (texto.equals("umafrase")) {
+           System.out.println("O texto é uma frase");
+       } else {
+           System.out.println("O texto não é uma frase");     
+       }
+    }
+}
