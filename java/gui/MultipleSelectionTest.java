d9f0e2e1e27c89c0522358f7cd909a7a823de6b7
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2022-04-05 09:57:31 -0300

    Multiple selection list sample #12

---
+++ java/gui/MultipleSelectionTest.java
@@ -0,0 +1,12 @@
+
+// Testando MultipleSelectionFrame.
+import javax.swing.JFrame;
+
+public class MultipleSelectionTest {
+    public static void main(String[] args) {
+        MultipleSelectionFrame multipleSelectionFrame = new MultipleSelectionFrame();
+        multipleSelectionFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        multipleSelectionFrame.setSize(350, 150);
+        multipleSelectionFrame.setVisible(true);
+    }
+} // fim da classe MultipleSelectionTest
