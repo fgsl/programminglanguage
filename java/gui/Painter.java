6ef64bfd31b9f672eb66ba370d438a445a22c383
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2022-05-17 18:42:59 -0300

    Painting with the mouse #17

---
+++ java/gui/Painter.java
@@ -0,0 +1,20 @@
+// Testando o PaintPanel.
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+public class Painter
+{
+    public static void main(String[] args)
+    {
+        // cria o JFrame
+        JFrame application = new JFrame("A simple paint program");
+        PaintPanel paintPanel = new PaintPanel();
+        application.add(paintPanel, BorderLayout.CENTER);
+        // cria um rótulo e o coloca em SOUTH do BorderLayout
+        application.add(new JLabel("Drag the mouse to draw"),
+        BorderLayout.SOUTH);
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.setSize(400, 200);
+        application.setVisible(true);
+    }
+} // fim da classe Painter
