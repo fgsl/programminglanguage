c850638742b1402be70dff5d4732aaebe23d6666
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2022-05-31 08:20:13 -0300

    Exemplo de Panel

---
+++ java/gui/PanelFrame.java
@@ -0,0 +1,26 @@
+// Utilizando um JPanel para ajudar a fazer o layout dos componentes.
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+public class PanelFrame extends JFrame
+{
+    private final JPanel buttonJPanel; // painel para armazenar botões
+    private final JButton[] buttons;
+    // construtor sem argumento
+    public PanelFrame()
+    {
+        super("Panel Demo");
+        buttons = new JButton[5];
+        buttonJPanel = new JPanel();
+        buttonJPanel.setLayout(new GridLayout(1, buttons.length));
+        // cria e adiciona botões
+        for (int count = 0; count < buttons.length; count++)
+        {
+            buttons[count] = new JButton("Button " + (count + 1));
+            buttonJPanel.add(buttons[count]); // adiciona botão ao painel
+        }
+        add(buttonJPanel, BorderLayout.SOUTH); // adiciona painel ao JFrame
+    }
+} // fim da classe PanelFrame
